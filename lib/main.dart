cb1a70368f435b6cbbe35effa41bec6158412e56
pawel.ataman <pawel.ataman@onwelo.com>
2023-07-10 22:24:54 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const YumYum());
+}
+
+class YumYum extends StatelessWidget {
+const YumYum({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
