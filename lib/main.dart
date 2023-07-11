e5a4ee7cb268a307eb1083eb4c84337ffcc49c4a
pawel.ataman <pawel.ataman@onwelo.com>
2023-07-11 16:35:48 +0200

    add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:yumyum/features/meal/presentation/pages/categories_page.dart';
+import 'package:yumyum/features/meal/presentation/pages/category_details_page.dart';
+import 'package:yumyum/features/meal/presentation/pages/meal_details_page.dart';
 
 void main() {
-  runApp(const YumYum());
+  runApp(YumYum());
 }
 
 class YumYum extends StatelessWidget {
-  const YumYum({Key? key}) : super(key: key);
-
+  YumYum({Key? key}) : super(key: key);
+  final ThemeData theme = ThemeData();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'YumYum',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Home(),
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          canvasColor: Colors.amber.shade50,
+          colorScheme: theme.colorScheme.copyWith(secondary: Colors.cyan),
+          textTheme: theme.textTheme.copyWith(
+            bodyLarge: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1)
+            ),
+            bodyMedium: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1)
+            ),
+            titleMedium: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold
+            )
+          )
+          ),
+      home: CategoriesPage(),
+      routes: {
+       CategoryDetailsPage.route:(context) => CategoryDetailsPage(),
+       MealDetailsPage.route:(context) => MealDetailsPage()
+      },
     );
   }
 }
