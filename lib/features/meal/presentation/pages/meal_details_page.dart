0a161e943d2e045a8bf3c6beb0cd96d6452bffed
pawel.ataman <pawel.ataman@onwelo.com>
2023-07-11 16:35:22 +0200

    implement meal detail page

---
+++ lib/features/meal/presentation/pages/meal_details_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/models/meal_model.dart';
+
+class MealDetailsPage extends StatelessWidget {
+  static const route = '/meal-details';
+const MealDetailsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    final Map<String,Object> routeData = ModalRoute.of(context)!.settings.arguments as Map<String,Object>;
+    final Meal meal = routeData['meal'] as Meal;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Container());
+  }
+}
