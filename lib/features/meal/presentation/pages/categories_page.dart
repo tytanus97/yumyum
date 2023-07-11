1ab4c54b21a820499fba3a70f96e2280aca97551
pawel.ataman <pawel.ataman@onwelo.com>
2023-07-11 16:35:00 +0200

    implement categories page

---
+++ lib/features/meal/presentation/pages/categories_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:yumyum/features/meal/presentation/widgets/category_item.dart';
+
+import '../../data/dummy_data.dart';
+import '../../domain/models/category_model.dart';
+
+class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GridView(
+          padding: const EdgeInsets.all(25),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES
+              .map((Category category) =>
+                  CategoryItem(color: category.color, title: category.title, id: category.id,))
+              .toList(),
+        ),
+        appBar: AppBar(
+          title: const Text('YumYum'),
+          backgroundColor: Colors.amber.withOpacity(.8),
+        ));
+  }
+}
